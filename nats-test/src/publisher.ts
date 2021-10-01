17f825380509626a1e763352a4590b280b378d37
Treflo <treflo@Treflos-MacBook-Pro.local>
2021-10-01 16:33:08 +0530

    New API for Order Service is created.

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -2,9 +2,7 @@ import nats from 'node-nats-streaming';
 
 
 async function start(){
-const stan = await nats.connect('ticketing', 'abc', {
-  url:"http://dev.treflo.com:4222",
-});
+const stan = nats.connect('ticketing', 'abc', {url: 'http://localhost:4222'} );
 
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
