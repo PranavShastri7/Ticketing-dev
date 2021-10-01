def43c0c50cad77083196b5bea87907fbc015106
Treflo <treflo@Treflos-MacBook-Pro.local>
2021-10-01 16:38:36 +0530

    New API for Order Service is created.

--- Orders/src/routes/new.ts
+++ Orders/src/routes/new.ts
@@ -27,16 +27,12 @@ router.post(
   async (req: Request, res: Response) => {
     const { ticketId } = req.body;
 
-    // Find the ticket the user is trying to order in the database
     const ticket = await Ticket.findById(ticketId);
     if (!ticket) {
       throw new NotFoundError();
     }
 
-    // Make sure that this ticket is not already reserved
-    // Run query to look at all orders.  Find an order where the ticket
-    // is the ticket we just found *and* the orders status is *not* cancelled.
-    // If we find an order from that means the ticket *is* reserved
+
     const existingOrder = await Order.findOne({
       ticket: ticket,
       status: {
@@ -51,11 +47,6 @@ router.post(
       throw new BadRequestError('Ticket is already reserved');
     }
 
-    // Calculate an expiration date for this order
-
-    // Build the order and save it to the database
-
-    // Publish an event saying that an order was created
 
     res.send({});
   }
